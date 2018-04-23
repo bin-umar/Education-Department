cd7abb472d6e90fa95d5e9baa60630858301687c
Чахонгир Тулфоров <jakha_96-96@mail.ru>
2018-04-23 11:06:25 +0500

    Reading subjects for extraction && Locking modifying

--- src/app/services/extraction.service.ts
+++ src/app/services/extraction.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import { ResponseExtractionSubject } from '../models/curriculum';
 
 @Injectable()
 export class ExtractionService {
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
+
+  getSubjectsByExtractionId(id: number) {
+    return this.auth.http.get(
+      this.auth.host + 'self.php?route=extractions&operation=one' +
+      '&id=' + id + '&token=' + this.auth.token
+    ).map((response: ResponseExtractionSubject) => {
+      return response;
+    });
+  }
 
 }
